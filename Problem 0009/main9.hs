39bbbd811fd1a44914c113365bd6036d5f305c89
Nayuki Minase <nayuki@eigenstate.org>
2013-03-16 15:41:43 +0000

    Tweaked minor things for clarity: whitespace, comments, redundant type.

--- Problem 0009/main9.hs
+++ Problem 0009/main9.hs
@@ -1,1 +1,1 @@
-main = print (head [a*b*c| a <- [1..500], b <- [1.. 500], let c = 1000 - a - b , a^2 + b^2 == c^2])
+main = print (head [a*b*c | a <- [1..500], b <- [1..500], let c = 1000 - a - b, a^2 + b^2 == c^2])
--- Problem 0016/main16.hs
+++ Problem 0016/main16.hs
@@ -1,2 +1,2 @@
 import Data.Char
-main = print (sum $ map digitToInt $ show (2^1000 :: Integer))
+main = print (sum $ map digitToInt $ show (2^1000))
