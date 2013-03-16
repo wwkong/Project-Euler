96419f45ca283d7575b234e9f611a39d74817849
William Kong <wwkong92@gmail.com>
2013-03-16 11:59:59 -0300

    Update main18.hs

--- Problem 0018/main18.hs
+++ Problem 0018/main18.hs
@@ -8,9 +8,9 @@ parseTree s = reverse $ map (map read) $ map words $ lines s
 findPath :: [[Int]] -> Int
 findPath tree
 	| length (tree !! 0) == 1 = tree !! 0 !! 0
-	| otherwise				  = findPath $ [(zipWith (+) 
-										             [max (tree !! 0 !! n) (tree !! 0 !! (n+1)) | n <- [0..((length (tree !! 0))-1)]] 
-												     (tree !! 1))] ++ tail (tail tree)											
+	| otherwise		  = findPath $ [(zipWith (+) 
+						[max (tree !! 0 !! n) (tree !! 0 !! (n+1)) | n <- [0..((length (tree !! 0))-1)]] 
+						(tree !! 1))] ++ tail (tail tree)											
 main = do
 	contents <- readFile "data.txt" 
 	let tData = parseTree contents
