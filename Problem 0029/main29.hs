2dede5e24299c139aa4a3193ca1db213b90d8754
William Kong <wwkong92@gmail.com>
2013-03-24 21:36:15 -0400

    Added Haskell solutions

---
+++ Problem 0029/main29.hs
@@ -0,0 +1,11 @@
+-- First application of functors and the Data.Set package
+
+import Control.Applicative
+import Data.Set
+
+-- Print and write out the result
+main = do
+		let perm = (^) <$> [2..100] <*> [2..100]
+		let ans = length $ toList $ fromList perm
+		writeFile "pe29.txt" $ show ans
+		print ans
