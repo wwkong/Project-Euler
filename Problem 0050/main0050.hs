2c4c6ef71377a0ce0895b8db71c9fa36350273d2
William Kong <wwkong92@gmail.com>
2013-04-06 22:00:54 -0400

    Added Haskell solutions.

--- Problem 0050/main0050.hs
+++ Problem 0050/main0050.hs
@@ -2,3 +2,32 @@
 Which prime, below one-million, can be written as the sum of the most consecutive primes?
 -}
 
+import Data.Numbers.Primes
+import Data.List
+
+-- Trial and error suggests a good lower bound is 120 and so we take all primes under 100000
+cPrimes = takeWhile (<10000) primes
+cPLens = length cPrimes
+
+-- Define a function to find the max of our consecutive prime sum from (cPrimes) for a parameter n
+-- that defines the number of consecutive entries
+getMaxConsec n = getMaxConsec' 0 cPrimes where
+	getMaxConsec' maxP lst
+		| sum consecLst > 10^6 = maxP
+		| len == n = max maxP consecP
+		| otherwise = getMaxConsec' (max maxP consecP) (tail lst)
+		where 
+			len = length lst
+			consecLst = take n lst
+			consecP = (if isPrime (sum consecLst) then sum consecLst else 0)
+			
+-- Import the tuple max function from PE 14
+tupMax a b
+	| max (fst a) (fst b) == fst a = a
+	| max (fst a) (fst b) == fst b = b
+			
+-- Print and write out the answer
+main = do
+		let ans = foldl1 tupMax [ (n, getMaxConsec n) | n <- [120.. cPLens] , getMaxConsec n /= 0]
+		writeFile "pe50.txt" $ show ans
+		print ans
---
+++ Problem 0055/main0055.hs
@@ -0,0 +1,18 @@
+-- How many Lychrel numbers are there below ten-thousand?
+-- We are given that 349, 47 are not Lychrel and 196, 10677 are
+
+-- Create a function to check if a number is Lychrel
+isLychrel n = isLychrel' n 0 where 
+	isLychrel' n iter
+		| iter == 50 = True
+		| nStr == reverse nStr && iter > 0 = False
+		| otherwise = isLychrel' (n + nRev) (iter + 1)
+		where 
+			nStr = show n
+			nRev = read (reverse nStr) :: Integer
+
+-- Print and write out the answer
+main = do
+		let ans = length [n | n <- [1.. (10^4-1)], isLychrel n]
+		writeFile "pe55.txt" $ show ans
+		print ans
