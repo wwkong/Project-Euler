050bcfdcc459425cbfd23428918791d2125026a8
William Kong <wwkong92@gmail.com>
2013-04-08 20:09:25 -0400

    Added Haskell solutions.

---
+++ Problem 0043/main0043.hs
@@ -0,0 +1,40 @@
+{-
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits
+0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+d2d3d4=406 is divisible by 2
+d3d4d5=063 is divisible by 3
+d4d5d6=635 is divisible by 5
+d5d6d7=357 is divisible by 7
+d6d7d8=572 is divisible by 11
+d7d8d9=728 is divisible by 13
+d8d9d10=289 is divisible by 17
+
+Find the sum of all 0 to 9 pandigital numbers with this property.
+-}
+
+import Data.List
+
+-- Get all pandigital numbers as strings
+panNums = permutations "0123456789"
+
+-- Check the divisibility condition for a panNum string
+isPanDiv :: String -> Bool
+isPanDiv nStr 
+	| (takeThree 2) `mod` 2 /= 0 = False
+	| (takeThree 3) `mod` 3 /= 0 = False
+	| (takeThree 4) `mod` 5 /= 0 = False
+	| (takeThree 5) `mod` 7 /= 0 = False
+	| (takeThree 6) `mod` 11 /= 0 = False
+	| (takeThree 7) `mod` 13 /= 0 = False
+	| (takeThree 8) `mod` 17 /= 0 = False
+	| otherwise = True
+	where takeThree n = (read :: String -> Integer) $ take 3 $ snd $ splitAt (n-1) nStr
+	
+-- Print and write out the answer
+main = do 
+		let ans = sum [read pNums :: Integer | pNums <- panNums , isPanDiv pNums]
+		writeFile "pe46.txt" $ show ans
+		print ans
---
+++ Problem 0049/main0049.hs
@@ -0,0 +1,43 @@
+{-
+The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual 
+in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are 
+permutations of one another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property,
+but there is one other 4-digit increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this sequence?
+-}
+
+import Data.Numbers.Primes
+import Data.List
+import qualified Data.Set as S
+
+-- Create a function to get all prime permutations of a number in ascending order
+pPerms n = 	let nStr = show n in 
+			sort $ filter isPrime $ map (read :: String -> Integer) $ permutations nStr
+
+-- Define a function that choose 3 distinct elements from a lst
+threeDistinct lst = (S.toList . S.fromList) [sort [a,b,c] | a <-lst, b <- lst \\ [a], c <- (lst \\ [a]) \\ [b]]
+
+-- Define a function to find a three element arithmetic subsequence in a list if it exists
+isArith lst = ((lst !! 1) - (lst !! 0)) == ((lst !! 2) - (lst !! 1))
+subArith lst 
+	| elemLst == [] = [] 
+	| otherwise = head elemLst 
+	where 
+		threeLst = threeDistinct lst
+		elemLst = filter isArith threeLst
+		
+-- Find all such triples by filtering throughly
+tLst =  filter (\lst -> isPrime (lst !! 0) && (lst !! 0) /= (lst !! 1)) $ 
+		(S.toList . S.fromList) $
+		filter (/=[]) 
+		[(subArith . pPerms) a | a <- takeWhile (<= 9997) $ dropWhile (<1000) primes]
+			
+
+-- Print and write out the answer
+main = do
+		let ans = tLst
+		writeFile "pe49.txt" $ show ans
+		print ans
