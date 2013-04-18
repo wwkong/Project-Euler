ad776135a1d2d41a94646e63aae41806501beaac
William Kong <wwkong92@gmail.com>
2013-04-17 20:04:08 -0400

    Added Haskell solutions.

--- Problem 0079/main0079.hs
+++ Problem 0079/main0079.hs
@@ -3,34 +3,37 @@
 -- Given that the three characters are always asked for in order, analyse the file keylog.txt so as to 
 -- determine the shortest possible secret passcode of unknown length.
 
+-- Note that the shortest length will have only one occurence of each digit in the set of digits
+
 import Data.List
+import Data.Char
 import Debug.Trace
 import qualified Data.Set as S
 
--- Define an ordered union operator for two list elements (B into A)
-ordUnion :: String -> String -> String
-ordUnion lstA lstB = ordUnion' lstA lstB [] where
-	ordUnion' lstA' lstB' acc
-		| lstB' == [] = acc
-		| trace ("bMatch=" ++ show bMatch ++ " restA=" ++ show restA) False = undefined
-		| otherwise = ordUnion' restA (tail lstB') (acc ++ (union [head lstB'] lstA'))
-		where 
-			bMatch = head $ (findIndices (==(head lstB')) lstA' ++ [(length lstA')-1])
-			restA = snd $ splitAt bMatch lstA'
+-- Given a set of numbers as strings, create a function to find the set of unique digits
+uniqueDigits lst = (S.toList . S.fromList) $ map digitToInt $ concat lst
 
--- Given a list of ordered strings, define a function that merges them in order
-mergeStrLst strLst = foldl1 union strLst
+-- Create a function that takes in a list of numbers, as strings, and a list of digits
+-- and returns the digit that exists in the head of each element in the list but nowhere else
+uniqueHeadDigits :: [Int] -> [String] -> [Int]
+uniqueHeadDigits digits numLst = 	[n | n <- digits, 
+									(n `elem` (map digitToInt $ map head numLst)) &&
+									(not $ n `elem` (uniqueDigits $ map tail numLst))]
 
--- Implement a tuple min function
--- Create a tuple max function
-tupMin a b
-	| min (fst a) (fst b) == fst a = a
-	| min (fst a) (fst b) == fst b = b
-
--- Print and write out the answer
+-- Create a function that solves our problem through wrapping the above function
+findPass digits numLst
+	-- | trace ("findPass " ++ show digits ++ " " ++ show numLst) False = undefined
+	| digits == [] = []
+	| curDigit == [] = findPass digits (map tail numLst)
+	| otherwise = curDigit ++ (findPass (digits \\ curDigit) modLst)
+	where
+		curDigit = uniqueHeadDigits digits numLst
+		modLst = map (\nStr -> if length nStr == 1 then nStr else nStr \\ (show curDigit)) numLst
+	
 main = do
-		--rawData <- readFile "keylog.txt"
-		--let contents = (S.toList . S.fromList . lines) rawData -- Remove duplicates
-		let ans = -- (read :: String -> Integer) $ mergeStrLst contents
+		rawData <- readFile "keylog.txt"
+		let contents = (S.toList . S.fromList . lines) rawData -- Remove duplicates
+		let ans = read $ map intToDigit $ findPass (uniqueDigits contents) contents :: Integer
 		writeFile "pe79.txt" $ show ans
 		print ans
+		
