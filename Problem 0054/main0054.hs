11d73b702d180db60f76ca5db9737ac659e6a32b
William Kong <wwkong92@gmail.com>
2013-04-24 22:35:20 -0300

    Update main0054.hs

--- Problem 0054/main0054.hs
+++ Problem 0054/main0054.hs
@@ -74,7 +74,7 @@ handRank hand
 	| 3 `elem` (pokerGroups hand) 						= 4 -- Three of a kind
 	| pokerGroups hand == [1,2,2] 						= 3 -- Two pair
 	| 2 `elem` (pokerGroups hand)						= 2 -- One pair
-	| otherwise														= 1 -- High card
+	| otherwise								= 1 -- High card
 	where handVals = S.fromList $ map fst hand
 
 -- Define a function that takes two poker hands and returns 1 if the first hand wins and 2 otherwise
